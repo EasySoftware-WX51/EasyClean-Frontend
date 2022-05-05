4be5dfd74ca07dba006cf4211e8bed453b1b7d85
Erick.BH <erickalonsobh@gmail.com>
2022-05-05 01:37:56 -0500

    updated service component and assets

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,38 +1,31 @@
 <template>
-    <v-container grid-list-md>
-        <h1 class="text-center">Services</h1>
-        <v-divider></v-divider>
-        <v-layout row wrap>
-            <v-flex xs12 mt-5 >
-                <h2 class="text-center">Cleaning companies available</h2>
-            </v-flex>
-            <v-flex xs12 sm6 mt-5 v-for="(item,index) in arrayCompanies" :key="index">
-                <v-card>
-                <v-card-title>{{item.title}}</v-card-title>
-                <v-img src="../assets/cleaning-company2.jpeg" height="150" contain></v-img>
-                <v-card-text>{{item.description}}.</v-card-text>
-                    <v-card-actions>
-                        <v-btn color="info" outlined><router-link to="/home" class="text-decoration-none">Aply</router-link></v-btn>
-                    </v-card-actions>
-                    </v-card>
-            </v-flex> 
-        </v-layout>
-    </v-container>
+  <v-container grid-list-md>
+    <h1 class="text-center">Services</h1>
+    <v-divider></v-divider>
+    <v-layout row wrap justify-center>
+      <v-flex xs12 lg8 mt-5 v-for="(item,index) in arrayServices" :key="index">
+        <v-img src="../assets/cleaning.png" height="500"></v-img>
+        <v-card class="mt-5">
+          <v-card-title >{{item.title}}</v-card-title>
+          <v-card-text>
+            <p>{{item.description}}</p>
+            <v-btn color="success" class="mr-3"><router-link class="white--text text-decoration-none" to='/servicecompanies'>Request</router-link></v-btn>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
-        data(){
-        return{
-            arrayCompanies:[
-                {id:1,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-                {id:2,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-                {id:3,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-                {id:4,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-                {id:5,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-                {id:6,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'}
-            ]
-        }
-    },
+  data(){
+    return{
+      arrayServices:[
+        {id:1,title: 'Cleaning', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
+        {id:2,title: 'Disinfection', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
+      ]
+    }
+  },
 }
 </script>
