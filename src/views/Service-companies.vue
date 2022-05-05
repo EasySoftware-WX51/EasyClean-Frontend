f9d7539b7296f511cd64ef765e4256202cc03922
Franco <vasquezemmanuel439@gmail.com>
2022-05-05 11:54:42 -0500

    Update Service-companies.vue

--- src/views/Service-companies.vue
+++ src/views/Service-companies.vue
@@ -1,57 +1,15 @@
 <template>
-  <v-container grid-list-md>
-    <h1 class="text-center">Services</h1>
-    <v-divider></v-divider>
-    <v-layout row wrap>
-      <v-flex xs12 mt-5 >
-        <h2 class="text-center">Cleaning companies available</h2>
-      </v-flex>
-      <v-flex xs12 sm6 mt-5 v-for="(item,index) in arrayCompanies" :key="index">
-        <v-card>
-          <v-card-title>{{item.title}}</v-card-title>
-          <v-img v-if="item.id==1" src="../assets/cleaning-company.jpeg" height="150" contain></v-img>
-          <v-img v-if="item.id==2" src="../assets/cleaning-company2.jpeg" height="150" contain></v-img>
-          <v-img v-if="item.id==3" src="../assets/cleaning-company3.jpeg" height="150" contain></v-img>
-          <v-img v-if="item.id==4" src="../assets/cleaning-company4.jpeg" height="150" contain></v-img>
-          <v-img v-if="item.id==5" src="../assets/cleaning-company5.jpeg" height="150" contain></v-img>
-          <v-img v-if="item.id==6" src="../assets/cleaning-company6.jpeg" height="150" contain></v-img>
-          <v-card-text>{{item.description}}.</v-card-text>
-          <v-card-actions>
-            <v-btn @click="doaRequest()" color="info" outlined>Aply</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-snackbar v-model="snackbar">
-      {{ mensaje }}
-      <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
-        Close
-      </v-btn>
-    </v-snackbar>
-  </v-container>
+  <servicecompanies/>
 </template>
 
 <script>
+import servicecompanies from '../components/component-service-companies'
+
 export default {
-  data(){
-    return{
-      arrayCompanies:[
-        {id:1,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-        {id:2,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-        {id:3,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-        {id:4,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-        {id:5,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-        {id:6,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'}
-      ],
-      snackbar: false,
-      mensaje: ''
-    }
+  name: 'Servicecompanies',
+
+  components: {
+    servicecompanies,
   },
-  methods:{
-    doaRequest(){
-      this.snackbar = true
-      this.mensaje= 'Youve made a request!'
-    },
-  }
 }
 </script>
