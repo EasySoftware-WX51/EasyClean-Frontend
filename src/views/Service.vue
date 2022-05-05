6f4f7e0db2783048139e4dbcce24f2d8ab769259
Franco <vasquezemmanuel439@gmail.com>
2022-05-05 11:54:00 -0500

    Update Service.vue

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -1,32 +1,15 @@
 <template>
-  <v-container grid-list-md>
-    <h1 class="text-center">Services</h1>
-    <v-divider></v-divider>
-    <v-layout row wrap justify-center>
-      <v-flex xs12 lg8 mt-5 v-for="(item,index) in arrayServices" :key="index">
-        <v-img v-if="item.id==1" src="../assets/cleaning.png" height="500"></v-img>
-        <v-img v-if="item.id==2" src="../assets/disinfection.png" height="500"></v-img>
-        <v-card class="mt-5">
-          <v-card-title >{{item.title}}</v-card-title>
-          <v-card-text>
-            <p>{{item.description}}</p>
-            <v-btn color="success" class="mr-3"><router-link class="white--text text-decoration-none" to='/servicecompanies'>Request</router-link></v-btn>
-          </v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <service/>
 </template>
 
 <script>
+import service from '../components/component-service'
+
 export default {
-  data(){
-    return{
-      arrayServices:[
-        {id:1,title: 'Cleaning', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-        {id:2,title: 'Disinfection', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
-      ]
-    }
+  name: 'Service',
+
+  components: {
+    service,
   },
 }
 </script>
