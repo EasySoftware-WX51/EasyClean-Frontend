088d2902f769779f37df652c97d9d2b893ff2d33
Williamz21 <u201816224@upc.edu.pe>
2022-05-04 23:05:35 -0500

    home.vue added and service.vue updated

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,39 @@
 <template>
     <v-container grid-list-md>
         <h1 class="text-center">Home</h1>
+        <v-divider></v-divider>
+        <v-layout row wrap>
+            <v-flex md6 mt-10>
+                <v-card class="mb-10" v-for="(item,index) in arrayRequest" :key="index">
+                    <v-row>
+                        <v-col cols="12" sm="4">
+                                <v-img src="../assets/cleaning-company2.jpeg" height="300" contain></v-img>
+                        </v-col>
+                        <v-col cols="12" sm="8">
+                            <v-card-title >{{item.title}} {{item.id}}</v-card-title>
+                            <v-card-text>
+                            <p>{{item.description}}</p>
+                            <v-btn color="success" class="mr-3">Pay</v-btn>
+                            <v-btn color="error">Cancel</v-btn>
+                            </v-card-text>
+                        </v-col>
+                    </v-row>
+                </v-card>
+
+            </v-flex>
+        </v-layout>
     </v-container>
 </template>
 
+<script>
+export default {
+    data(){
+        return{
+            arrayRequest:[
+                {id:1,title:'Request', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. Doloremque maxime hic deserunt alias voluptatem similique! Praesentium. Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. Doloremque maxime hic deserunt alias voluptatem similique! Praesentium. Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.'},
+                {id:2,title:'Request', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. Doloremque maxime hic deserunt alias voluptatem similique! Praesentium. Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. Doloremque maxime hic deserunt alias voluptatem similique! Praesentium. Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.'}
+            ]
+        }
+    },
+}
+</script>
--- src/views/Service.vue
+++ src/views/Service.vue
@@ -3,84 +3,36 @@
         <h1 class="text-center">Services</h1>
         <v-divider></v-divider>
         <v-layout row wrap>
-            <v-flex xs12 mt-5>
+            <v-flex xs12 mt-5 >
                 <h2 class="text-center">Cleaning companies available</h2>
             </v-flex>
-            <v-flex xs12 sm6 mt-5>
+            <v-flex xs12 sm6 mt-5 v-for="(item,index) in arrayCompanies" :key="index">
                 <v-card>
-                <v-card-title>Lorem, ipsum dolor.</v-card-title>
+                <v-card-title>{{item.title}}</v-card-title>
                 <v-img src="../assets/cleaning-company2.jpeg" height="150" contain></v-img>
-                <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.</v-card-text>
+                <v-card-text>{{item.description}}.</v-card-text>
                     <v-card-actions>
                         <v-btn color="info" outlined><router-link to="/" class="text-decoration-none">Aply</router-link></v-btn>
                     </v-card-actions>
                     </v-card>
             </v-flex> 
-
-            <v-flex xs12 sm6 mt-5>
-                <v-card>
-                <v-card-title >Lorem, ipsum dolor.</v-card-title>
-                <v-img src="../assets/cleaning-company2.jpeg" height="150" contain></v-img>
-                <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.</v-card-text>
-                    <v-card-actions>
-                            <div class="text-center">
-                                <v-btn color="info" outlined><router-link to="/" class="text-decoration-none">Aply</router-link></v-btn>
-                            </div>
-                        </v-card-actions>
-                    </v-card>
-            </v-flex> 
-
-            <v-flex xs12 sm6 mt-5>
-                <v-card>
-                <v-card-title >Lorem, ipsum dolor.</v-card-title>
-                <v-img src="../assets/cleaning-company2.jpeg" height="150" contain></v-img>
-                <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.</v-card-text>
-                    <v-card-actions>
-                            <div class="text-center">
-                                <v-btn color="info" outlined><router-link to="/" class="text-decoration-none">Aply</router-link></v-btn>
-                            </div>
-                        </v-card-actions>
-                    </v-card>
-            </v-flex> 
-            
-            <v-flex xs12 sm6 mt-5>
-                <v-card>
-                <v-card-title >Lorem, ipsum dolor.</v-card-title>
-                <v-img src="../assets/cleaning-company2.jpeg" height="150" contain></v-img>
-                <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.</v-card-text>
-                    <v-card-actions>
-                            <div class="text-center">
-                                <v-btn color="info" outlined><router-link to="/" class="text-decoration-none">Aply</router-link></v-btn>
-                            </div>
-                        </v-card-actions>
-                    </v-card>
-            </v-flex>
-
-            <v-flex xs12 sm6 mt-5>
-                <v-card>
-                <v-card-title >Lorem, ipsum dolor.</v-card-title>
-                <v-img src="../assets/cleaning-company2.jpeg" height="150" contain></v-img>
-                <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.</v-card-text>
-                    <v-card-actions>
-                            <div class="text-center">
-                                <v-btn color="info" outlined><router-link to="/" class="text-decoration-none">Aply</router-link></v-btn>
-                            </div>
-                        </v-card-actions>
-                    </v-card>
-            </v-flex> 
-            
-            <v-flex xs12 sm6 mt-5>
-                <v-card>
-                <v-card-title >Lorem, ipsum dolor.</v-card-title>
-                <v-img src="../assets/cleaning-company2.jpeg" height="150" contain></v-img>
-                <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.</v-card-text>
-                    <v-card-actions>
-                            <div class="text-center">
-                                <v-btn color="info" outlined><router-link to="/" class="text-decoration-none">Aply</router-link></v-btn>
-                            </div>
-                        </v-card-actions>
-                    </v-card>
-            </v-flex>
         </v-layout>
     </v-container>
 </template>
+
+<script>
+export default {
+        data(){
+        return{
+            arrayCompanies:[
+                {id:1,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
+                {id:2,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
+                {id:3,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
+                {id:4,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
+                {id:5,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'},
+                {id:6,title: 'Lorem, ipsum dolor.', description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore, nostrum.'}
+            ]
+        }
+    },
+}
+</script>
