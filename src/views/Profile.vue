577a1e9e85c67bbe9b2775acf7e0ac66cda13ffb
Franco <vasquezemmanuel439@gmail.com>
2022-05-05 11:53:15 -0500

    Update Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,71 +1,15 @@
 <template>
-    <v-container grid-list-md>
-        <h1 class="text-center">Profile</h1>
-        <v-divider></v-divider>
-        <v-layout row wrap justify-center>
-            <v-flex xs12 lg8 mt-10>
-                <v-card>
-                    <v-row wrap>
-                        <v-col cols="12" sm="4">
-                            <v-img src="../assets/profileimg.png" height="300" contain></v-img>
-                        </v-col>
-
-                        <v-col cols="12" sm="4">
-                            <v-card-title>Hospital Sierra Madre's information</v-card-title>
-                            <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                            Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                            </v-card-text>
-
-                            <v-card-title>Contact</v-card-title>
-                            <v-card-text>
-                            <ul>
-                                <li>hospitalSM@gmail.com</li>
-                                <li>999999999</li>
-                                <li>01 932300200</li>
-                            </ul>
-                            </v-card-text>
-
-                        </v-col>
-
-                        <v-col cols="12" sm="4">
-                            <v-card-title>Adress</v-card-title>
-                            <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                            Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                            Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                            </v-card-text>
-                        </v-col>
-                    </v-row>
-                </v-card>
-            </v-flex> 
-
-            <v-flex xs12 lg8 mt-10>
-                <v-card>
-                    <v-card-title>Details</v-card-title>
-                    <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    Tempore esse sit quae blanditiis accusantium commodi tenetur natus magnam officiis reiciendis, optio est. 
-                    Doloremque maxime hic deserunt alias voluptatem similique! Praesentium.
-                    </v-card-text>
-                </v-card>
-            </v-flex> 
-        </v-layout>
-    </v-container>
+  <profile/>
 </template>
+
+<script>
+import profile from '../components/component-profile'
+
+export default {
+  name: 'Profile',
+
+  components: {
+    profile,
+  },
+}
+</script>
